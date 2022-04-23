936fa5cd5e5943e3f3a927d6cef79eb43a06930f
KEATH <9940145+keath_zl@user.noreply.gitee.com>
2022-04-23 21:39:59 +0800

    make grids

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import './home.scss';
 import headerBg from '../images/bigScreen.png'
 
+
 export const Home = () => {
   return (
     <div className='home' >
-      <header style={{backgroundImage:`${(headerBg)}`}}/>
+      <header style={{backgroundImage:`url(${headerBg})`}}/>
+      <main>
+        <section className="section1">1</section>
+        <section className="section2">1</section>
+        <section className="section3">1</section>
+        <section className="section4">1</section>
+        <section className="section5">1</section>
+      </main>
     </div>
   );
 };
