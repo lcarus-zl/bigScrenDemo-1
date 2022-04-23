0e2dba2197a6db1b06b9604aa4f050dae81d5683
KEATH <9940145+keath_zl@user.noreply.gitee.com>
2022-04-23 21:08:37 +0800

    add px function

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,6 @@ cd dist &&
 git init &&
 git add . &&
 git commit -m deploy &&
-git remote add origin git@github.com:FrankFang/Se1uqKnmKDZS.git &&
+git remote add origin git@github.com:lcarus-zl/bigScrenDemo-1.git &&
 git push -uf origin main:gh-pages &&
 cd -;
